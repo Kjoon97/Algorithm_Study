6f20fd45d360c09cc76b153eeba6663b0ac91b0a
joonhyuck <jh83370@naver.com>
2022-03-09 18:47:19 +0900

    침몰하는 타이타닉(그리디- deque 이용)

--- 섹션 4/8. 침몰하는 타이타닉/AA.py
+++ 섹션 4/8. 침몰하는 타이타닉/AA.py
@@ -1,8 +1,10 @@
 import sys
+from collections import deque
 #sys.stdin = open("input.txt", "r")
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 a.sort()
+a = deque(a)
 cnt=0
 while a:                     #리스트가 빌 때까지 반복
     if len(a)==1:
@@ -10,7 +12,7 @@ while a:                     #리스트가 빌 때까지 반복
         break
     if a[0]+a[-1] <= m:
         cnt+=1
-        a.pop(0)
+        a.popleft()
         a.pop()
     else:
         cnt+=1
@@ -19,5 +21,5 @@ print(cnt)
 
 '''
 pop(0)을 하면 앞에 원소 제거되면서 뒤의 원소들이 한칸씩 앞으로 재정렬하는데
-비효율적이다.
+비효율적이다. -> 포인터로 가리키는 deque를 이용한다.
 '''
