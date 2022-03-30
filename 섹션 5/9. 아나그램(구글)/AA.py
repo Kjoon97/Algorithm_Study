2696d43df0c4a72872798579a1dc78966efbd824
joonhyuck <jh83370@naver.com>
2022-03-30 14:32:51 +0900

    아나그램(리스트 해쉬)

--- 섹션 5/9. 아나그램(구글)/AA.py
+++ 섹션 5/9. 아나그램(구글)/AA.py
@@ -1,6 +1,41 @@
 import sys
-#sys.stdin = open("input.txt",'r')
+#sys.stdin = open("in1.txt",'r')
+#==================================c++처럼 풀어보기 리스트 해시 방법 =============
+a= input()
+b= input()
+str1= [0]*52   #알파벳이 소문자, 대문자 합해서 총 52개
+str2= [0]*52
 
+for x in a:
+    if x.isupper():
+        str1[ord(x)-65]+=1
+    else:
+        str1[ord(x)-71]+=1
+        
+for x in b:
+    if x.isupper():
+        str2[ord(x)-65]+=1
+    else:
+        str2[ord(x)-71]+=1
+
+for i in range(52):             #파이썬은 if str1==str2 이렇게 직접 비교해도됨.
+    if str1[i]!=str2[i]:
+        print("NO")
+        break
+else:
+    print("YES")
+
+
+#문자를 아스키 넘버로 변환해주는 함수 ord(x)
+#대문자 아스키 번호 : 65~90 (A~Z)-> str1,str2에 넣게 되면 -65해서 인덱스0~25에 넣기.
+#소문자 아스키 번호 : 97~122 (a~z)-> str1,str2에 넣게 되면 -71해서 인덱스26~51에 넣기. 
+        
+
+
+
+
+'''
+===========================딕셔너리 해시 방법=================================
 firstw = input()
 secondw = input()
 sH = dict()
@@ -19,3 +54,4 @@ else:
     print("YES")
 
 #딕셔너리 내장 함수 - 딕셔너리.get('A',0) + 1 => 딕셔너리에 키 'A'의 value값을 가져와라, value가 없으면 0을 리턴해라. 
+'''
