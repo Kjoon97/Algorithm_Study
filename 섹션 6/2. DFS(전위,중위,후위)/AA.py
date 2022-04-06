6a27286a2ade45431a7358afac4b40ca5f39812b
joonhyuck <jh83370@naver.com>
2022-04-06 23:02:51 +0900

    DFS(전위,중위,후위)

---
+++ 섹션 6/2. DFS(전위,중위,후위)/AA.py
@@ -0,0 +1,16 @@
+import sys
+#sys.stdin = open("input.txt",'r')
+
+def DFS(v):
+    if v>7:
+        return
+    else:
+        print(v, end = ' ')
+        
+        DFS(v*2)    #왼쪽
+        
+        DFS(v*2+1)  #오른쪽
+
+
+if __name__=="__main__":
+    DFS(1)
